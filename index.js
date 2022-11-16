45b16581c28b2f9028a01435c1aea563b2d7d6fc
Githmin Jayawardhana <githmin21@gmail.com>
2022-11-16 08:58:40 +0530

    added mongodb connection

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const mongoose = require('mongoose')
+
+
+mongoose.connect(process.env.mongodb).then(() => {
+    console.log("Connected to DB")
+}).catch((err) => {
+    console.log(err)
+})
 
 
 
-app.get('/', (req,res)=>{
+
+app.get('/', (req, res) => {
     res.send("Hello")
 })
 
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT, () => {
     console.log(`Serving on port ${process.env.PORT}`)
 })
